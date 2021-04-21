140960b0f6254d011b7c47a2b8c1fafd205ebfa3
kzachary <kzachary@villanova.edu>
2021-04-21 16:27:44 -0400

    added half of best player data

---
+++ best-player-data-2.sql
@@ -0,0 +1,132 @@
+
+--fname, lname, cyear, ppg
+
+--missouri
+INSERT INTO BEST_PLAYER VALUES('Dru', 'Smith', 'Senior', 14);
+
+--GT
+
+INSERT INTO BEST_PLAYER VALUES('Moses', 'Wright', 'Senior', 17);
+
+--Wisconsin
+
+INSERT INTO BEST_PLAYER VALUES('D''Mitrik', 'Trice', 'Senior', 14);
+
+--Maryland
+
+INSERT INTO BEST_PLAYER VALUES('Eric', 'Ayala', 'Junior', 15);
+
+--Bonnies
+
+INSERT INTO BEST_PLAYER VALUES('Kyle', 'Loften', 'Junior', 14);
+
+--VT
+
+INSERT INTO BEST_PLAYER VALUES('Keve', 'Aluma', 'Junior', 15);
+
+--VCU
+
+INSERT INTO BEST_PLAYER VALUES('Nah''Shon', 'Hyland', 'Sophmore', 20);
+
+--Rutgers
+
+INSERT INTO BEST_PLAYER VALUES('Ron', 'Harper Jr', 'Junior', 15);
+
+--Cuse
+
+INSERT INTO BEST_PLAYER VALUES('Buddy', 'Boeheim', 'Junior', 18);
+
+--Utah st
+
+INSERT INTO BEST_PLAYER VALUES('Neemias', 'Queta', 'Junir', 15);
+
+--UCLA
+
+INSERT INTO BEST_PLAYER VALUES('Johnny', 'Juzang', 'Sophmore', 16);
+
+--ORegon st
+
+INSERT INTO BEST_PLAYER VALUES('Ethan', 'Thompson', 'Senior', 16);
+
+--Georgetown
+
+INSERT INTO BEST_PLAYER VALUES('Jahvon', 'Blair', 'Senior', 15);
+
+--Drake
+
+INSERT INTO BEST_PLAYER VALUES('Joseph', 'Yesufu', 'Sophmore', 13);
+
+--Winthrop
+
+INSERT INTO BEST_PLAYER VALUES('Chandler', 'Vaudrin', 'Senior', 12);
+
+--UCSB
+
+INSERT INTO BEST_PLAYER VALUES('Jaquori', 'Mclaughlin', 'Senior', 16);
+
+--Ohio
+
+INSERT INTO BEST_PLAYER VALUES('Jason', 'Preston', 'Junior', 16);
+
+--North Texas
+
+INSERT INTO BEST_PLAYER VALUES('Javion', 'Hamlet', 'Senior', 16);
+
+--Liberty
+
+INSERT INTO BEST_PLAYER VALUES('Darius', 'Mcghee', 'Junior', 16);
+
+--UNC Greensboro
+
+INSERT INTO BEST_PLAYER VALUES('Isiah', 'Miller', 'Senior', 19);
+
+--Abilene Christian
+
+INSERT INTO BEST_PLAYER VALUES('Kolton', 'Kohl', 'Senior', 12);
+
+--Morehead St
+
+INSERT INTO BEST_PLAYER VALUES('Johni', 'Broome', 'Freshman', 14);
+
+--Colgate
+
+INSERT INTO BEST_PLAYER VALUES('Jordan', 'Burns', 'Senior', 17);
+
+--Eastern Washington
+
+INSERT INTO BEST_PLAYER VALUES('Tanner', 'Groves', 'Junior', 17);
+
+--Grand Canyon
+
+INSERT INTO BEST_PLAYER VALUES('Asbjorn', 'Midtgaard', 'Senior', 14);
+
+--Cleveland St
+
+INSERT INTO BEST_PLAYER VALUES('Torrey', 'Patton', 'Senior', 15);
+
+--Oral Roberts
+
+INSERT INTO BEST_PLAYER VALUES('Max', 'Abmas', 'Sophmore', 25);
+
+--Iona
+
+INSERT INTO BEST_PLAYER VALUES('Isaiah', 'Ross', 'Senior', 18);
+
+--Drexel
+
+INSERT INTO BEST_PLAYER VALUES('Camren', 'Wynter', 'Junior', 16);
+
+--Hartford
+
+INSERT INTO BEST_PLAYER VALUES('Austin', 'Williams', 'Senior', 14);
+
+--Texas Southern
+
+INSERT INTO BEST_PLAYER VALUES('Michael', 'Weathers', 'Senior', 17);
+
+--Norfolk st
+
+INSERT INTO BEST_PLAYER VALUES('Devante', 'Carter', 'Senior', 15);
+
+
+
