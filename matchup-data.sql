33e4f5239acb9cb1805f9cebf4cc91368ce4e220
WesMaster7 <aeetim77@gmail.com>
2021-04-20 20:21:00 -0400

    Matchup and region DATA
    Inserted the data for matchup and region.
    Matchup is done only for the first round.

---
+++ matchup-data.sql
@@ -0,0 +1,111 @@
+INSERT INTO MATCHUP VALUES
+('Baylor','Hartford','South','-25.5','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('North Carolina','Wisconsin','South','-1.5','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Villanova','Winthrop','South','-6.5','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Purdue','North Texas','South','-7.5','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Texas Tech','Utah State','South','-4','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Arkansas','Colgate','South','-8.5','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Florida','Virginia Tech','South','0','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Ohio State','Oral Roberts','South','-16','19/03/2021');
+
+---%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+INSERT INTO MATCHUP VALUES
+('Illinois','Drexel','Midwest','-22.5','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Loyola Chicago','Georgia Tech','Midwest','-5.5','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Tennessee','Oregon State','Midwest','-9','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Oklahoma State','Liberty','Midwest','-7.5','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('San Diego State','Syracuse','Midwest','-3','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('West Virginia','Morehead State','Midwest','-13','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Clemson','Rutgers','Midwest','-1','19/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Houston','Cleveland State','Midwest','-20','19/03/2021');
+
+--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+INSERT INTO MATCHUP VALUES
+('Michigan','Texas Southern','East','-25','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('LSU','St. Bonaventure','East','-1.5','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Colorado','Georgetown','East','-6.5','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Florida State','UNC Greensboro','East','-10.5','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('BYU','UCLA','East','-3.5','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Texas','Abilene Christian','East','-9','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('UConn','Maryland','East','-3','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Alabama','Iona','East','-17','20/03/2021');
+
+--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+INSERT INTO MATCHUP VALUES
+('Gonzaga','Norfolk State','West','-33','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Oklahoma','Missouri','West','-1','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Creighton','UC Sanata Barbara','West','-7','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Virginia','Ohio','West','-7.5','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('USC','Drake','West','-7','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Kansas','Eastern Washington','West','-10.5','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Oregon','VCU','West','-5','20/03/2021');
+
+INSERT INTO MATCHUP VALUES
+('Iowa','Grand Canyon','West','-14.5','20/03/2021');
+
+
+
+
+
+
+
+
+
+
+
+
