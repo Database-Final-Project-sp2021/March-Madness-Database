90e04df775c683950968de5c7b62ab8da9af9199
natewiley11 <43258343+natewiley11@users.noreply.github.com>
2021-04-25 16:21:12 -0400

    Update March-Madness-Queries.sql

--- March-Madness-Queries.sql
+++ March-Madness-Queries.sql
@@ -1,2 +1,11 @@
 /*Queries to demonstrate how the March Madness database works*/
 
+
+/*Get the top 10 scorers in college basketball*/
+select * from best_player order by ppg desc fetch first 10 rows only;
+
+/*Get the opponent of every number 5 seed team*/
+select m.collegelowseed as "Five Seeds", m.collegehighseed as Opponent
+from team t, matchup m
+where t.college = m.collegelowseed
+and m.collegelowseed in (select college from team where seednum=5);
